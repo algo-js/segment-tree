99da494985d8beff3f275081c0755ad7a2a76354
Alexander Belov <asb@rdw.ru>
2019-08-30 19:20:24 +0300

    [ci skip] Updates

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"128":"Class"},"rows":[{"id":0,"kind":128,"name":"SegmentTree","url":"classes/segmenttree.html","classes":"tsd-kind-class"}]};
